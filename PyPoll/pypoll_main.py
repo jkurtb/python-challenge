abfd4534fb1c4dcca6fe6539ef7bcb86e1e9c325
jkurtbaltero <balterojkurt@gmail.com>
2020-06-01 03:09:56 -0400

    winner determined & results printed

--- PyPoll/pypoll_main.py
+++ PyPoll/pypoll_main.py
@@ -4,10 +4,12 @@ import csv
 #Set a relative file path
 csvpath = os.path.join("Resources", "election_data.csv")
 
-#Define initial counter & empty dictionary
+#Define initial counter & empty dictionaries
 total_votes = 0
 candidate_info = {}
 percent_vote = {}
+winner = None
+winner_vote = 0
 
 
 #Open/read csv & skip headers
@@ -16,7 +18,7 @@ with open(csvpath) as csvfile:
     header = next(csvfile)
 
 #Loop through rows to get the sum of all votes
-    #then add items [Candidate: Votes] in the dictionary 
+    #then add items in the dictionary 
     for row in csvreader:
         total_votes = total_votes + 1
         if row[2] in candidate_info.keys():
@@ -27,8 +29,11 @@ with open(csvpath) as csvfile:
     for key, value in candidate_info.items():
         percent_vote[key] = "{:.3f}".format((value/total_votes)*100)
 
-
-
+#Determine the winner based on highest votes
+    for key in candidate_info.keys():
+        if candidate_info[key] > winner_vote:
+            winner = key
+            winner_vote = candidate_info[key]
 
 #Print analysis/results to GitBash
 print("Election Results")
@@ -37,5 +42,8 @@ print(f"Total Votes: {total_votes}")
 print("---------------------------")
 for key, value in candidate_info.items():
     print(f"{key}: {percent_vote[key]}% ({value})")
+print("---------------------------")
+print(f"Winner: {winner}")
+print("---------------------------")
 
 
