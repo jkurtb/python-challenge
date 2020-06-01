84fcce71c97ca202f42a7a65042c10341a4061d1
jkurtbaltero <balterojkurt@gmail.com>
2020-06-01 02:18:05 -0400

    added comments, percentage value format with 3 decimals

--- PyPoll/pypoll_main.py
+++ PyPoll/pypoll_main.py
@@ -1,7 +1,7 @@
 #Import modules
 import os
 import csv
-#Set the file path
+#Set a relative file path
 csvpath = os.path.join("Resources", "election_data.csv")
 
 #Define initial counter & empty dictionary
@@ -15,21 +15,22 @@ with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvfile)
 
-#Loop through rows to add items in the dictionary
+#Loop through rows to get the sum of all votes
+    #then add items [Candidate: Votes] in the dictionary 
     for row in csvreader:
         total_votes = total_votes + 1
         if row[2] in candidate_info.keys():
             candidate_info[row[2]] += 1
         else:
             candidate_info[row[2]] = 1
-
+#Calculate each candidate's percentage vote with 3 decimal places
     for key, value in candidate_info.items():
         percent_vote[key] = "{:.3f}".format((value/total_votes)*100)
 
 
 
 
-#Print analysis & results 
+#Print analysis/results to GitBash
 print("Election Results")
 print("---------------------------")
 print(f"Total Votes: {total_votes}")
